4580cec2a9b1bcb553f2cdaf1699e460976789b0
Michal Migurski <mike@remix.com>
2018-03-02 15:05:14 -0800

    Stubbed in setuptools package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'sharedstreets',
+    version = '0.0.0',
+    author = 'Kevin Webb, Denis Carriere, Michal Migurski',
+    description = 'Python implementation of SharedStreets Reference System',
+    license = 'MIT',
+    keywords = 'sharedstreets nacto openstreetmap map graph street',
+    url = 'https://github.com/sharedstreets/sharedstreets-python',
+    packages = ['sharedstreets'],
+)
