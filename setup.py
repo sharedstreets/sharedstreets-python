b2a70dd72d556774b75096be26b85c1416f1fd50
Michal Migurski <mike@remix.com>
2018-04-26 14:06:52 -0700

    Seeking to deploy from Python 3.6 only

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sharedstreets',
-    version = '0.0.0rc1',
+    version = '0.0.0rc2',
     author = 'Kevin Webb, Denis Carriere, Michal Migurski',
     description = 'Python implementation of SharedStreets Reference System',
     license = 'MIT',
