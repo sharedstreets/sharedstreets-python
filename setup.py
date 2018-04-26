fabae7d312a1df0d4d5309b67a08000960e9b5a8
Michal Migurski <mike@remix.com>
2018-04-26 14:32:06 -0700

    Bumped version slightly to trigger rebuild

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sharedstreets',
-    version = '0.1.0',
+    version = '0.1.1',
     author = 'Kevin Webb, Denis Carriere, Michal Migurski',
     description = 'Python implementation of SharedStreets Reference System',
     license = 'MIT',
