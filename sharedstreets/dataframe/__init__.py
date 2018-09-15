ab8008d60028679be91dc6a038e8223bb2b8d27a
Michal Migurski <mike@teczno.com>
2018-09-14 22:08:21 -0700

    Extensive cleanup and comments

--- sharedstreets/dataframe/__init__.py
+++ sharedstreets/dataframe/__init__.py
@@ -19,43 +19,43 @@ class _Feature:
             'geometry': {'type': type, 'coordinates': coordinates},
             }
 
-def _combine_geometries(minlon, minlat, maxlon, maxlat, geometry_frames):
-    ''' Clip bbox from multiple geometry dataframes and return their union.
+def _union_dataframes(dataframe1, dataframe2):
+    ''' Return union of two DataFrames with no repeated IDs.
     
-        Ensure that IDs aren't repeated so GeoDataFrame.append() does not complain.
+        Ensure that IDs aren't repeated so DataFrame.append() does not complain.
     '''
-    def union_geoframes(frame1, frame2):
-        ''' Return union of two frames with no repeated IDs.
-        '''
-        seen_ids, new_rows = set(frame1.index), list()
-    
-        for (id, row) in frame2.iterrows():
-            if id not in seen_ids:
-                seen_ids.add(id)
-                new_rows.append(row)
-        
-        return frame1.append(new_rows, verify_integrity=True)
-    
-    # Union all frames together after using GeoDataFrame.cx to clip bounds
-    return functools.reduce(union_geoframes, [
-        frame.cx[minlon:maxlon, minlat:maxlat] for frame in geometry_frames
-        ])
+    seen_ids, new_rows = set(dataframe1.index), list()
 
-def _collect_intersections(ids, intersection_frames):
-    ''' Collect named intersections from multiple dataframes into one dataframe.
+    for (id, row) in dataframe2.iterrows():
+        if id not in seen_ids:
+            seen_ids.add(id)
+            new_rows.append(row)
+    
+    return dataframe1.append(new_rows, verify_integrity=True)
+
+def _combine_geodataframes(minlon, minlat, maxlon, maxlat, geodataframes):
+    ''' Clip bbox from multiple GeoDataFrames and return their union.
+
+    Union all frames together after using GeoDataFrame.cx to clip bounds
+    '''
+    return functools.reduce(_union_dataframes,
+        [frame.cx[minlon:maxlon, minlat:maxlat] for frame in geodataframes])
+
+def _collect_dataframe_rows(ids, dataframes):
+    ''' Collect identified rows from multiple DataFrames into one and return it.
     '''
     remaining_ids, new_rows = set(ids), list()
     
-    for frame in intersection_frames:
-        for (id, row) in frame.iterrows():
+    for dataframe in dataframes:
+        for (id, row) in dataframe.iterrows():
             if id in remaining_ids:
                 remaining_ids.remove(id)
                 new_rows.append(row)
     
     # Initialize an empty frame that new rows can be appended to
-    empty_frame = intersection_frames[0].iloc[0:0]
+    empty_dataframe = dataframes[0].iloc[0:0]
     
-    return empty_frame.append(new_rows, verify_integrity=True)
+    return empty_dataframe.append(new_rows, verify_integrity=True)
 
 def get_bbox(minlon, minlat, maxlon, maxlat, data_url_template=None):
     ''' Get a single Frames instance of SharedStreets entities in an area.
@@ -68,21 +68,15 @@ def get_bbox(minlon, minlat, maxlon, maxlat, data_url_template=None):
         in itertools.product(range(ul.x, lr.x+1), range(ul.y, lr.y+1))
         ]
     
-    geometries_frame = _combine_geometries(minlon, minlat, maxlon, maxlat,
+    geometries = _combine_geodataframes(minlon, minlat, maxlon, maxlat,
         [frames.geometries for frames in tile_frames])
     
-    logging.debug(geometries_frame.dtypes)
-    logging.debug(geometries_frame.shape)
-    
-    ids = set(geometries_frame.fromIntersectionId) | set(geometries_frame.toIntersectionId)
+    ids = set(geometries.fromIntersectionId) | set(geometries.toIntersectionId)
 
-    intersections_frame = _collect_intersections(ids,
+    intersections = _collect_dataframe_rows(ids,
         [frames.intersections for frames in tile_frames])
 
-    logging.debug(intersections_frame.dtypes)
-    logging.debug(intersections_frame.shape)
-    
-    return Frames(intersections_frame, geometries_frame)
+    return Frames(intersections, geometries)
 
 def get_tile(*args, **kwargs):
     ''' Get a single Frames instance for a tile of SharedStreets entities.
